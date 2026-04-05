06aa2e7ebeea5b3673b822f3efa7beb0ca9824e7
ShivamBarot2602 <74816448+ShivamBarot2602@users.noreply.github.com>
2026-04-05 09:52:35 +0530

    Add: valid-word-abbreviation - submission-0

---
+++ Data Structures & Algorithms/valid-word-abbreviation/submission-0.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string} word
+     * @param {string} abbr
+     * @return {boolean}
+     */
+    validWordAbbreviation(word, abbr) {
+    let n=word.length;
+    let m=abbr.length;
+    let i=0;
+    let j=0;
+
+    while(i < n && j < m) {
+        if(abbr[j] === "0") return false;
+
+        if(isNaN(abbr[j])) {
+            if(word[i] === abbr[j]) {
+                i++;
+                j++;
+            } else {
+                return false
+            }
+        } else {
+            let subLen = 0;
+            while(j < m && !isNaN(abbr[j]) && abbr[j] !== " ") {
+                subLen = subLen * 10 + parseInt(abbr[j]);
+                j++;
+            }
+            i+= subLen
+        }
+    }
+
+    return i === n && j === m
+    }
+}
