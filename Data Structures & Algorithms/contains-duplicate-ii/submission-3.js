b39a51034f325f80930adc9fa1522c78fe97729b
ShivamBarot2602 <74816448+ShivamBarot2602@users.noreply.github.com>
2026-04-09 17:49:31 +0530

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.js
@@ -0,0 +1,61 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {boolean}
+     */
+    // containsNearbyDuplicate(nums, k) {
+    //     let set = new Set();
+    //     let l=0;
+
+    //     for(let r=0; r<nums.length; r++) {
+    //         if(r-l > k) {
+    //             set.delete(nums[l]);
+    //             l++;
+    //         }
+    //         if(set.has(nums[r])) {
+    //             return true
+    //         }
+
+    //         set.add(nums[r])
+    //     }
+
+    //     return false
+    // }
+
+    // containsNearbyDuplicate(nums, k) {
+    //     let window = new Set();
+    //     let l=0;
+
+    //     for(let r=0; r<nums.length; r++) {
+    //         if(r-l > k) {
+    //             window.delete(nums[l]);
+    //             l++;
+    //         }
+    //         if(window.has(nums[r])) {
+    //             return true
+    //         }
+    //         window.add(nums[r])
+    //     }
+
+    //     return false
+    // }
+
+    containsNearbyDuplicate(nums, k) {
+        let l=0;
+        let set = new Set();
+
+        for(let r=0; r<nums.length; r++) {
+            if(r-l > k) {
+                set.delete(nums[l]);
+                l++;
+            }
+            if(set.has(nums[r])) {
+                return true;
+            }
+            set.add(nums[r]);
+        }
+
+        return false
+    }
+}
