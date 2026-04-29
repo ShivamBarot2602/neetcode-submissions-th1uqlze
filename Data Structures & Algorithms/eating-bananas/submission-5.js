5c274cec43edaf63fb68de925b45d662dfeacfb8
ShivamBarot2602 <74816448+ShivamBarot2602@users.noreply.github.com>
2026-04-29 21:38:17 +0530

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.js
@@ -0,0 +1,55 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    // minEatingSpeed(piles, h) {
+    //     let left = 0;
+    //     let right = Math.max(...piles);
+    //     let res = Number.POSITIVE_INFINITY;
+
+    //     while(left <= right) {
+    //         let k = Math.trunc((left+right) / 2);
+    //         let sum = 0;
+
+    //         for(let i=0; i<piles.length; i++) {
+    //             sum += Math.ceil(piles[i] / k);
+    //         }
+
+    //         if(sum <= h) {
+    //             right = k - 1;
+    //             res = Math.min(res, k);
+    //         } else {
+    //             left = k+1
+    //         }
+    //     }
+        
+    //     return res
+    // }
+
+    minEatingSpeed(piles, h) {
+        let l=1;
+        let r=Math.max(...piles);
+        let res = Number.POSITIVE_INFINITY;
+
+        while(l<=r) {
+            let k=Math.trunc((l+r)/2);
+            let sum=0;
+
+            for(let i=0; i<piles.length; i++) {
+                sum+=Math.ceil(piles[i]/k)
+            }
+
+            if(sum <= h) {
+                r=k-1;
+                res=Math.min(k, res)
+            } else {
+                l=k+1;
+            }
+        }
+        console.log("Res ",res)
+        return res
+    }
+
+}
