418b2bb70bb1d7d48eabb9c2f3e5a2a288b8d43e
ShivamBarot2602 <74816448+ShivamBarot2602@users.noreply.github.com>
2026-05-16 16:49:51 +0530

    Add: intersection-of-two-linked-lists - submission-2

---
+++ Data Structures & Algorithms/intersection-of-two-linked-lists/submission-2.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {ListNode} headA
+     * @param {ListNode} headB
+     * @return {ListNode}
+     */
+    getIntersectionNode(headA, headB) {
+        let ptr1 = headA;
+        let ptr2 = headB;
+
+        while(ptr1 !== ptr2) {
+            ptr1 = ptr1 ? ptr1.next : headB;
+            ptr2 = ptr2 ? ptr2.next : headA;
+        }
+
+        return ptr1
+    }
+
+    // Hashset Solution
+    //  getIntersectionNode(headA, headB) {
+    //     let set = new Set();
+    //     let curr = headA;
+
+    //     while(curr) {
+    //         set.add(curr);
+    //         curr=curr.next;
+    //     }
+
+    //     curr = headB;
+
+    //     while(curr) {
+    //         if(set.has(curr)) {
+    //             return curr;
+    //         }
+    //         curr=curr.next
+    //     }
+
+    //     return null
+    //  }
+}
