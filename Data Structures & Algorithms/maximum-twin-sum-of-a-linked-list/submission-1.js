817e9b3799ee03ec30eddb9b379c86f385bbc64b
ShivamBarot2602 <74816448+ShivamBarot2602@users.noreply.github.com>
2026-05-18 08:39:27 +0530

    Add: maximum-twin-sum-of-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/maximum-twin-sum-of-a-linked-list/submission-1.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {number}
+     */
+    pairSum(head) {
+        let slow=head;
+        let fast=head;
+
+        while(fast && fast.next.next) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        let second = slow.next;
+        let prev = null;
+        let curr = second;
+
+        while(curr) {
+            let temp = curr.next;
+            curr.next = prev;
+            prev=curr;
+            curr=temp;
+        }
+
+        let ptr = head;
+        let res=0;
+
+        while(prev) {
+            res = Math.max(res, ptr.val + prev.val)
+            ptr=ptr.next;
+            prev=prev.next;
+        }
+
+        return res
+    }
+}
