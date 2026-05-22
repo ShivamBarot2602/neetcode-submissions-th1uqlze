d03e77fef6d484c56a2112a8d47d5ef7d1ba15ec
ShivamBarot2602 <74816448+ShivamBarot2602@users.noreply.github.com>
2026-05-22 08:37:21 +0530

    Add: design-linked-list - submission-10

---
+++ Data Structures & Algorithms/design-linked-list/submission-10.js
@@ -0,0 +1,138 @@
+class Node {
+    constructor(val, next) {
+        this.val=val;
+        this.next=next || null;
+    }
+}
+
+class MyLinkedList {
+    constructor() {
+        this.head=null;
+        this.tail=null;
+        this.size=0;
+    }
+
+    /**
+     * @param {number} index
+     * @return {ListNode}
+     */
+    // getPrev(index) {
+
+    // }
+
+    /**
+     * @param {number} index
+     * @return {number}
+     */
+    get(index) {
+        console.log("Called")
+        // console.log("Head ",this.head, this.size)
+        if(this.size <= 0 || index >= this.size || this.size === 0) {
+            return -1
+        } else {
+            let curr=this.head;
+            for(let i=0; i<index;i++) {
+                curr=curr.next;
+            }
+        
+            // console.log("Size ",this.size)
+            // console.log("get ",curr)
+            return curr.val
+        }
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    addAtHead(val) {
+        let node = new Node(val);
+        if(this.size === 0) {
+            this.head= node;
+            this.tail = node;
+        } else {
+            node.next=this.head;
+            this.head = node;
+        }
+        // console.log("Add t head ",this.head)
+       
+        this.size+=1;
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    addAtTail(val) {
+        const node = new Node(val);
+
+        if(this.size === 0) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.size+=1;
+    }
+
+    /**
+     * @param {number} index
+     * @param {number} val
+     * @return {void}
+     */
+    addAtIndex(index, val) {
+        if(index < 0 || index > this.size) {
+            return 
+        }
+        let node = new Node(val);
+        if(index === 0) {
+            node.next = this.head;
+            this.head = node;
+            if(this.size === 0) {
+                this.tail = node;
+            }
+            this.size += 1
+        } else if(index === this.size) {
+            this.addAtTail(val)
+        } else {
+            let curr = this.head;
+
+            for(let i=0; i<index-1; i++) {
+                curr = curr.next;
+            }
+            let temp = curr.next;
+            curr.next = node;
+            node.next = temp;
+            this.size += 1
+        }
+    }
+
+    /**
+     * @param {number} index
+     * @return {void}
+     */
+    deleteAtIndex(index) {
+        if(index >= this.size || index < 0) {
+            return
+        } else if(index === 0) {
+            this.head = this.head?.next || null;
+            this.size -= 1;
+        } else if(this.size === 1 && index === 0) {
+            this.head = null;
+            this.tail = null;
+            this.size -= 1;
+        } else {
+            let curr = this.head;
+            for(let i=0; i<index-1; i++) {
+                curr=curr.next;
+            }
+            let removeNode = curr.next;
+            curr.next = removeNode?.next || null;
+            if(removeNode === this.tail) {
+                this.tail = curr;
+            }
+            this.size-=1;
+        }
+    }
+}
