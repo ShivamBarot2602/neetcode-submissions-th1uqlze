ec26377bc14f693b1a6c654043157f9ae5367f5b
ShivamBarot2602 <74816448+ShivamBarot2602@users.noreply.github.com>
2026-05-25 22:45:25 +0530

    Add: reorder-linked-list - submission-2

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-2.js
@@ -0,0 +1,77 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {void}
+     */
+    // reorderList(head) {
+    //     let slow = head;
+    //     let fast = head;
+
+    //     while(fast && fast.next) {
+    //         fast = fast.next.next;
+    //         slow = slow.next;
+    //     }
+
+    //     // Beginning of the second half of the list
+    //     let second = slow.next;
+    //     let prev = null;
+    //     slow.next = null;
+
+    //     while(second) {
+    //         let temp = second.next;
+    //         second.next = prev;
+    //         prev = second;
+    //         second = temp;
+    //     }
+
+    //     let first = head;
+    //     second = prev;
+
+    //     while(second) {
+    //         let temp1 = first.next;
+    //         let temp2 = second.next;
+    //         first.next = second;
+    //         second.next = temp1;
+    //         first = temp1;
+    //         second = temp2;
+    //     }
+    // }
+
+
+    // Recursive Solution
+    reorderList(head) {
+        return this.recursive(head, head.next);
+    }
+
+    recursive(root, curr) {
+        if(curr === null) {
+            return root;
+        }
+
+        root = this.recursive(root, curr.next);
+        if(root === null) {
+            return null;
+        }
+        let temp = null;
+
+        if(curr === root || root.next === curr) {
+            curr.next = null;
+        } else {
+            temp = root.next;
+            root.next = curr;
+            curr.next = temp;
+        }
+
+        return temp
+    }
+}
