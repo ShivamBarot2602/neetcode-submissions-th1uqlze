1e877ab0b942a46d81fb1cc9a7d196cd37cde563
ShivamBarot2602 <74816448+ShivamBarot2602@users.noreply.github.com>
2026-07-22 22:28:54 +0530

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.js
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    // maxArea(heights) {
+    //     let res = 0;
+    //     let L = 0;
+    //     let R = heights.length-1;
+
+    //     while(L < R) {
+    //         let width = R-L;
+    //         let height = Math.min(heights[L], heights[R]);
+    //         let area = width * height;
+    //         res = Math.max(res, area);
+    //         if(heights[L] < heights[R]) {
+    //             L++;
+    //         } else {
+    //             R--;
+    //         }
+    //     }
+    //     return res
+    // }
+
+    maxArea(heights) {
+        let L = 0;
+        let R = heights.length-1;
+        let max = 0;
+
+        while(L < R) {
+            let length = Math.abs(R - L);
+            let breadth = Math.min(heights[L], heights[R]);
+            let area = length*breadth;
+            max = Math.max(max, area);
+
+            if(heights[L] < heights[R]) {
+                L++;
+            } else {
+                R--;
+            }
+        }
+
+        return max
+    }
+}
