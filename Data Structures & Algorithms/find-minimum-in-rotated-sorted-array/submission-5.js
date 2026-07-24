7568d090901d5e697f108e249c69afce208b21a7
ShivamBarot2602 <74816448+ShivamBarot2602@users.noreply.github.com>
2026-07-24 23:26:34 +0530

    Add: find-minimum-in-rotated-sorted-array - submission-5

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-5.js
@@ -0,0 +1,121 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    // findMin(nums) {
+    //     let left = 0;
+    //     let right = nums.length-1;
+    //     let res = nums[0];
+
+    //     while(left <= right) {
+    //         if(nums[left] < nums[right]) {
+    //             res = Math.min(res, nums[left]);
+    //             break;
+    //         }
+
+    //         let mid = Math.trunc((left+right) / 2);
+    //         res = Math.min(res, nums[mid]);
+
+    //         if(nums[mid] >= nums[left]) {
+    //             left = mid+1;
+    //         } else {
+    //             right = mid-1;
+    //         }
+    //     }
+
+    //     return res
+    // }
+
+    // findMin(nums) {
+    //     let l=0;
+    //     let r=nums.length-1;
+    //     let res=nums[0];
+
+    //     while(l<=r) {
+    //         if(nums[l] < nums[r]) {
+    //             res = Math.min(res, nums[l]);
+    //         }
+
+    //         let mid = Math.trunc((l+r) / 2);
+    //         res = Math.min(res, nums[mid]);
+
+    //         if(nums[mid] >= nums[l]) {
+    //             l = mid+1
+    //         } else {
+    //             r = mid-1
+    //         }
+    //     }
+
+    //     return res
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// findMin(nums) {
+//     let l=0, r=nums.length-1;
+//     let res = Number.POSITIVE_INFINITY;
+
+//     while(l <= r) {
+//         if(nums[l] <= nums[r]) {
+//             res = Math.min(res, nums[l]);
+//             break
+//         }
+
+//         let mid = Math.trunc((l+r)/2);
+//         console.log("Mid ",mid)
+//         res = Math.min(res, nums[mid]);
+
+//         console.log("Res ",res)
+
+//         if(nums[l] <= nums[mid]) {
+//             l=mid+1
+//         } else {
+//             r=mid-1
+//         }
+//     }
+//     return res
+// }
+
+
+findMin(nums) {
+    let L=0;
+    let R=nums.length-1;
+    let res = nums[0];
+
+    while(L <= R) {
+        if(nums[L] < nums[R]) {
+            res = Math.min(res, nums[L]);
+            break;
+        }
+
+        let mid = Math.trunc((L + R) / 2);
+        res = Math.min(res, nums[mid])
+
+        if(nums[L] <= nums[mid]) {
+            L = mid+1;
+        } else {
+            R = mid-1;
+        }
+    }
+
+    return res
+}
+}
