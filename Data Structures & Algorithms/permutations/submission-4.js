9dbfc764b35360a6de6af71417b6efddd1a96f59
ShivamBarot2602 <74816448+ShivamBarot2602@users.noreply.github.com>
2026-08-22 13:34:55 +0530

    Add: permutations - submission-4

---
+++ Data Structures & Algorithms/permutations/submission-4.js
@@ -0,0 +1,91 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    // Recursive sol
+    // permute(nums) {
+    //     const generatePermutations = (i) => {
+    //         if(i >= nums.length) {
+    //             return [[]]
+    //         }
+
+    //         let resPerms = [];
+    //         let perms = generatePermutations(i+1);
+
+    //         for(let p of perms) {
+    //             for(let j=0; j<p.length+1; j++) {
+    //                 let pCopy = [...p];
+    //                 pCopy.splice(j, 0, nums[i]);
+    //                 resPerms.push(pCopy);
+    //             }
+    //         }
+
+    //         return resPerms
+    //     }
+
+    //     return generatePermutations(0)
+    // }
+
+    // Iterative sol
+    // permute(nums) {
+    //     let perms = [[]];
+
+    //     for(let i=0; i<nums.length; i++) {
+    //         let nextPerms = [];
+    //         for(let p of perms) {
+    //             for(let j=0; j<p.length+1; j++) {
+    //                 let pCopy = [...p];
+    //                 pCopy.splice(j, 0, nums[i]);
+    //                 nextPerms.push(pCopy);
+    //             }
+    //         }
+    //         perms = nextPerms
+    //     }
+
+    //     return perms
+    // }
+
+
+
+    // permute(nums) {
+    //     const generatePermutations = (i) => {
+    //         if(i >= nums.length) {
+    //             return [[]]
+    //         }
+    //         let resPerms = [];
+    //         let perms = generatePermutations(i+1);
+
+    //         for(let perm of perms) {
+    //             for(let j=0; j<perm.length+1; j++) {
+    //                 let pCopy = [...perm];
+    //                 pCopy.splice(j, 0, nums[i]);
+    //                 resPerms.push(pCopy);
+    //             }
+    //         }
+            
+    //         return resPerms;
+    //     }
+
+    //     return generatePermutations(0);
+    // }
+
+    // Iterative sol
+    permute(nums) {
+        let perms = [[]];
+
+        for(let num of nums) {
+            let nextPerms = []
+            for(let perm of perms) {
+                for(let j=0; j<perm.length+1; j++) {
+                        let pCopy = [...perm];
+                        pCopy.splice(j, 0, num);
+                        nextPerms.push(pCopy);
+                }
+                perms = nextPerms 
+            }
+        }
+
+        return perms
+    }
+}
