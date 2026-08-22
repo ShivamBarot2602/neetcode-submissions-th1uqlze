cfc6bef1cba06f75dc6675bdfc13c395da91cc55
ShivamBarot2602 <74816448+ShivamBarot2602@users.noreply.github.com>
2026-08-22 11:19:07 +0530

    Add: combination-target-sum - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum/submission-3.js
@@ -0,0 +1,84 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    // combinationSum(nums, target) {
+    //     let res = [];
+
+    //     const generateCombinations = (i, res, curr, sum) => {
+    //         if(sum === target) {
+    //             res.push([...curr]);
+    //             return;
+    //         }
+
+    //         if(i >= nums.length || sum > target) {
+    //             return;
+    //         }
+
+    //         curr.push(nums[i]);
+    //         generateCombinations(i, res, curr, sum+nums[i]);
+
+    //         curr.pop();
+    //         generateCombinations(i+1, res, curr, sum)
+    //     }
+
+    //     generateCombinations(0, res, [], 0);
+    //     return res
+    // }
+
+    // combinationSum(nums, target) {
+    //     let res = [];
+    //     let combs = [];
+    //     let sum = 0;
+
+    //     const generateCombinations = (sum, i) => {
+    //         if(sum === target) {
+    //             res.push([...combs]);
+    //             return;
+    //         }
+
+    //         for(let j=i; j<nums.length; j++) {
+    //             if(sum + nums[j] > target) continue;
+
+    //             combs.push(nums[j]);
+    //             generateCombinations(sum+nums[j], j);
+    //             combs.pop(); 
+    //         }
+    //     }
+
+    //     generateCombinations(sum, 0)
+    //     return res;
+    // }
+
+
+    combinationSum(nums, target) {
+        let res = [];
+        let combs = [];
+        let sum = 0;
+
+        const generateCombinations = (i, sum) => {
+            if(sum === target) {
+                res.push([...combs]);
+                return;
+            }
+            if(i >= nums.length) {
+                return
+            }
+
+            for(let j=i; j<nums.length; j++) {
+                if(sum + nums[j] > target) {
+                    continue
+                }
+
+                combs.push(nums[j]);
+                generateCombinations(j, sum + nums[j]);
+                combs.pop()
+            }
+        }
+
+        generateCombinations(0, sum);
+        return res;
+    }
+}
