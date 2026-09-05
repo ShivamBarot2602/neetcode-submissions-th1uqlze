25d122d95a71d38eae222eca8c43e6921618c92e
ShivamBarot2602 <74816448+ShivamBarot2602@users.noreply.github.com>
2026-09-05 17:17:05 +0530

    Add: flood-fill - submission-3

---
+++ Data Structures & Algorithms/flood-fill/submission-3.js
@@ -0,0 +1,58 @@
+class Solution {
+    /**
+     * @param {number[][]} image
+     * @param {number} sr
+     * @param {number} sc
+     * @param {number} color
+     * @return {number[][]}
+     */
+    // floodFill(image, sr, sc, color) {
+    //     let ROW=image.length;
+    //     let COL=image[0].length;
+    //     let directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+    //     let originalColor = image[sr][sc];
+
+    //     if(image[sr][sc] === color) return image
+
+    //     const dfs = (r, c) => {
+    //         if(r<0 || c<0 || r===ROW || c===COL || image[r][c] !== originalColor) {
+    //             return
+    //         }
+
+    //         image[r][c] = color;
+
+    //         for(let [dr, dc] of directions) {
+    //             let nr=r+dr; 
+    //             let nc=c+dc;
+
+    //             dfs(nr, nc);
+    //         }
+    //     }
+
+    //     dfs(sr, sc);
+    //     return image
+    // }
+
+    floodFill(image, sr, sc, color) {
+        let ROW=image.length;
+        let COL=image[0].length;
+
+        let originalColor = image[sr][sc];
+        if(originalColor === color) return image;
+
+        const dfs = (r, c) => {
+            if(r<0 || r===ROW || c<0 || c===COL || image[r][c] !== originalColor) {
+                return
+            }
+
+            image[r][c] = color;
+            dfs(r+1, c);
+            dfs(r-1, c);
+            dfs(r, c+1);
+            dfs(r, c-1);
+        }
+
+        dfs(sr, sc);
+        return image
+    }
+}
